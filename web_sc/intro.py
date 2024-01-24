8089e9620886308c9ae06711f347ba22dd9ae81a
faith mueni <muenifaith1000@gmail.com>
2024-01-24 17:05:17 +0300

    initial comment

--- web_sc/intro.py
+++
@@ -1,3 +0,0 @@
-#a tool used to get data from the web.. 
-# you use two libraries. 1. request library- allows  us to send HTTP requests. 2. Beautiful soup
-#pip install requests beautifulsoup4
---
+++ web_scraping/scraping.py
@@ -0,0 +1,30 @@
+#a tool used to get data from the web.. 
+# you use two libraries. 1. request library- allows  us to send HTTP requests. 2. Beautiful soup
+#pip install requests beautifulsoup4
+
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+url = "https://www.nytimes.com/"
+
+response = requests.get (url)
+
+#print (response.content)
+
+soup = BeautifulSoup (response.content, 'html.parser')
+
+articles = soup.find_all ('article', class_='css-8atqhb')
+data = []
+
+for article in articles [:10]:
+    title = article.find ('h2', class_='css-1vvhd4r').text.strip()
+    description = article.find ('p', class_='css-1echdzn').text.strip()
+    data.append ({'title':title,'description': description})
+
+with open ('nytimes.csv', 'w', newline = '') as file:
+    writer = csv.DictWriter(file, fieldnames = ['title','description'])
+    writer.writeheader()
+    writer.writerows (data)
+
+    print (soup)
